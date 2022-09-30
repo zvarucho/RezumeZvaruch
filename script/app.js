ec99e82697f89620e51cc13592d6868688b0a73a
zvarucho <100859123+zvarucho@users.noreply.github.com>
2022-09-30 13:36:07 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -72,3 +72,29 @@ function progress4(){
 
 }
 progress4();
+
+const submit = document.getElementById("enter");
+
+submit.addEventListener("click", function(e) {
+    if (confirm("Дійсно відправити цю форму?")){
+        alert("Ваші дані надіслано!");
+    }
+    else {
+    alert("Дані не надіслано");
+}
+});
+
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
